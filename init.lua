ab218fb9cff301839677af992e5ea42927b2276a
mike <mikehall@mikehall94.com>
2024-09-28 16:42:56 -0600

    syncing properly to codeberg

--- init.lua
+++ init.lua
@@ -1,6 +1,47 @@
 -- Grab configuration files from the zenzilla database
-require("zenzilla")
+require("config.lazy")
+require("config.maps")
+require("lazy").setup("plugins")
 
--- Show relative line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- global dictionaries can be accessed via vim.*
+-- g for global, b for buffer, w for window, t for tab, v for variables, o for options, etc.
+-- vim.opt is a global variable that allows you to set vim options
+
+-- :help lua-vim-variables
+-- :help vim-variables
+-- :help set-option
+-- :help vim.opt
+
+-- show line numbers and make them relative
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- set tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+-- turn of swap files and setup undodir
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- incremental search highlights matches as you type
+vim.opt.incsearch = true
+
+-- ensure good colors
+vim.opt.termguicolors = true
+
+-- add an 8 char buffer to the bottom of the viewport except for eof when scrolling
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- set update time
+vim.opt.updatetime = 50
