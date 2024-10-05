980aad78d76cd63cc91375ce57e14bc6aefba6aa
mike <mikehall@mikehall94.com>
2024-10-05 15:58:14 -0600

    transition maps to keymaps, utilizing the lua api

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -7,140 +7,120 @@
 
 -- ══ TODO: ═══════════════════════════════════════════════════════════
 --
--- 1. Add descriptions to all keymaps. Done.
--- 2. Add more keymaps
+-- 4. Add shortcuts to common directories, such as nvim, dotfiles, Zealot, etc.
 
+
+-- ══ LunarVim ════════════════════════════════════════════════════════
+-- -- Move current line / block with Alt-j/k ala vscode.
+-- ["<A-j>"] = "<Esc>:m .+1<CR>==gi",
+-- -- Move current line / block with Alt-j/k ala vscode.
+-- ["<A-k>"] = "<Esc>:m .-2<CR>==gi",
 
 vim.g.mapleader = " " -- Set space as the leader key
 
 
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-local keymap = vim.keymap.set
+local map = vim.api.nvim_set_keymap -- the old way of setting keymaps
+local keymap = vim.keymap.set -- the new way of setting keymaps
+
 
 -- Save
-map("n", "<leader>w", "<CMD>update<CR>", { desc = "Save" })
+keymap("n", "<leader>w", "<CMD>update<CR>", { desc = "Save" })
 
 -- Quit
-map("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit" })
+keymap("n", "<leader>q", "<CMD>qa<CR><CR>", { desc = "Quit" })
 
 -- Exit insert mode
-map("i", "jk", "<ESC>", { noremap = true, silent = true })
+keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 -- NeoTree
-map("n", "<leader>e", "<CMD>Neotree toggle<CR>", { desc = "Toggle NeoTree" })
-map("n", "<leader>r", "<CMD>Neotree focus<CR>", { desc = "Focus on NeoTree" })
+keymap("n", "<leader>e", "<CMD>Neotree toggle<CR>", { desc = "Toggle NeoTree" })
+keymap("n", "<leader>r", "<CMD>Neotree focus<CR>", { desc = "Focus on NeoTree" })
 
 -- Tabs
-map("n", "<leader><tab><tab>", "<CMD>tabnew<CR>", { desc = "New tab" })
-map("n", "<leader><tab>d", "<CMD>tabclose<CR>", { desc = "Close tab" })
-map("n", "<leader>t", "<CMD>+tabnext<CR>", { desc = "Next tab" })
-map("n", "<leader>pt", "<CMD>-tabnext<CR>", { desc = "Previous tab" })
-
--- Substitute.nvim
--- keymap.set("n", "s", require('substitute').operator, { noremap = true })
--- keymap.set("n", "ss", require('substitute').line, { noremap = true })
--- keymap.set("n", "S", require('substitute').eol, { noremap = true })
--- keymap.set("x", "s", require('substitute').visual, { noremap = true })
---
--- Substitute operator
--- keymap.set("n", "<leader>s", require('substitute').operator, { noremap = true, desc = "Substitute operator" })
---
--- -- Substitute the whole line
--- keymap.set("n", "<leader>ss", require('substitute').line, { noremap = true, desc = "Substitute whole line" })
---
--- -- Substitute to the end of the line
--- keymap.set("n", "<leader>S", require('substitute').eol, { noremap = true, desc = "Substitute to end of line" })
---
--- -- Substitute visually selected text
--- keymap.set("x", "<leader>s", require('substitute').visual, { noremap = true, desc = "Substitute visual selection" })
+keymap("n", "<leader><tab><tab>", "<CMD>tabnew<CR>", { desc = "New tab" })
+keymap("n", "<leader><tab>d", "<CMD>tabclose<CR>", { desc = "Close tab" })
+keymap("n", "<leader>t", "<CMD>+tabnext<CR>", { desc = "Next tab" })
+keymap("n", "<leader>pt", "<CMD>-tabnext<CR>", { desc = "Previous tab" })
 
 
 -- Undotree
-map("n", "<leader>u", "<CMD>UndotreeToggle<CR>", { desc = "Toggle undotree" })
+keymap("n", "<leader>u", "<CMD>UndotreeToggle<CR>", { desc = "Toggle undotree" })
 
 -- New Windows
-map("n", "<leader>o", "<CMD>vsplit<CR>", { desc = "Open a new window vertically" })
-map("n", "<leader>h", "<CMD>split<CR>", { desc = "Open a new window horizontally" })
+keymap("n", "<leader>ow", "<CMD>vsplit<CR>", { desc = "Open a new window vertically" })
+keymap("n", "<leader>h", "<CMD>split<CR>", { desc = "Open a new window horizontally" })
 
 
 -- Window Navigation
-map("n", "<A-h>", "<C-w>h", { desc = "Navigate to the left window" })
-map("n", "<A-l>", "<C-w>l", { desc = "Navigate to the right window" })
-map("n", "<A-k>", "<C-w>k", { desc = "Navigate to the window above" })
-map("n", "<A-j>", "<C-w>j", { desc = "Navigate to the window below" })
+keymap("n", "<A-h>", "<C-w>h", { desc = "Navigate to the left window" }) -- hint: use thumb to press alt
+keymap("n", "<A-l>", "<C-w>l", { desc = "Navigate to the right window" }) -- hint: use thumb to press alt
+keymap("n", "<A-k>", "<C-w>k", { desc = "Navigate to the window above" }) -- hint: use thumb to press alt
+keymap("n", "<A-j>", "<C-w>j", { desc = "Navigate to the window below" }) -- hint: use thumb to press alt
 
 -- Resize Windows
-map("n", "<C-Left>", "<C-w><", { desc = "Resize window to the left" })
-map("n", "<C-Right>", "<C-w>>", { desc = "Resize window to the right" })
-map("n", "<C-Up>", "<C-w>+", { desc = "Resize window up" })
-map("n", "<C-Down>", "<C-w>-", { desc = "Resize window down" })
+keymap("n", "<C-Left>", "<C-w><", { desc = "Resize window to the left" })
+keymap("n", "<C-Right>", "<C-w>>", { desc = "Resize window to the right" })
+keymap("n", "<C-Up>", "<C-w>+", { desc = "Resize window up" })
+keymap("n", "<C-Down>", "<C-w>-", { desc = "Resize window down" })
 
 -- Indent Blocks
-map('v', '<', '<gv', opts)
-map('v', '>', '>gv', opts)
+keymap('v', '<', '<gv')
+keymap('v', '>', '>gv')
 
 -- Jump back to file directory
-map("n", "<leader>pv", "<CMD>Ex<CR>", { desc = "Jump back to file directory" })
-map("n", "-", "<CMD>Ex<CR>", { desc = "Jump back to file directory" })
+keymap("n", "<leader>pv", "<CMD>Ex<CR>", { desc = "Jump back to file directory" })
+keymap("n", "-", "<CMD>Ex<CR>", { desc = "Jump back to file directory" })
 
 
 -- Helix inspired remaps because I never liked ^ and $
-map("n", "gh", "^", { desc = "[G]oto first char in line (h is chosen because its also a left movement" })
-map("n", "gl", "$", { desc = "[G]oto last char in line (l is chosen because its also a right movement" })
+keymap("n", "gh", "^", { desc = "[G]oto first char in line (h is chosen because its also a left movement" })
+keymap("n", "gl", "$", { desc = "[G]oto last char in line (l is chosen because its also a right movement" })
 
 -- "Drag" selected lines up and down
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Drag selection down one line" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Drag selection up one line" })
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Drag selection down one line" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Drag selection up one line" })
 
 
 -- Keep cursor in place as you are appending rows
-map("n", "J", "mzJ`z", { desc = "Keep cursor in place when appending rows" })
-
--- Keep cursor in the middle of the screen when scrolling with d, u
--- map("n", "<C-d>", "<C-d>zz", { desc = "Keep cursor in the middle of the screen when scrolling down" })
--- map("n", "<C-u>", "<C-u>zz", { desc = "Keep cursor in the middle of the screen when scrolling up" })
+keymap("n", "J", "mzJ`z", { desc = "Keep cursor in place when appending rows" })
 
 -- keep search terms in the middle of the screen
-map("n", "n", "nzzzv", { desc = "Keep next search term in the middle of the screen" })
-map("n", "N", "Nzzzv", { desc = "Keep previous search terms in the middle of the screen" })
+keymap("n", "n", "nzzzv", { desc = "Keep next search term in the middle of the screen" })
+keymap("n", "N", "Nzzzv", { desc = "Keep previous search terms in the middle of the screen" })
 
 -- yanks into a secondary buffer so you dont lose your yank
 -- map("x", "<leader>p", [["_dP]], { desc = "Yank into an alternate buffer so it doesnt get lost" })
 
--- cred to asbjornHaland
--- Yank to system clipboard 
-map("v", "<leader>y", '"+y', { desc = "Yank selection to system clipboard" })
-map("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+-- Yank to system clipboard
+keymap("v", "<leader>y", '"+y', { desc = "Yank selection to system clipboard" }) --asbjornHaland
+keymap("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" }) --asbjornHaland
 
 -- Delete without yanking
--- map("n", "v", "<leader>d", [["_d]], { desc = "Delete without yanking" })
+keymap({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
 
 -- Look this up laterhttps://crates.io/crates/tmux-sessionizer
 -- map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Open tmux-sessionizer" })
 
 -- LSP
--- map("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat file using LSP" })
-map("n", "<leader>fp", "<cmd>!black %<CR>", { desc = "[F]ormat [P]ython file" });
-map("n", "<leader>fj", "<cmd>%!jq '.'<CR>", { desc = "[F]ormat [J]son file" });
+keymap("n", "<leader>fp", "<cmd>!black %<CR>", { desc = "[F]ormat [P]ython file" });
 
 -- TODO: learn how to use marks/tags?
--- map("n", "<C-k>", "<cmd>cnext<CR>zz")
--- map("n", "<C-j>", "<cmd>cprev<CR>zz")
--- map("n", "<leader>k", "<cmd>lnext<CR>zz")
--- map("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Make current file executable
--- map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }, { desc = "Add executable permissions to the current buffer" })
+-- keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }, { desc = "Add executable permissions to the current buffer" })
 
 -- Replace the word under cursor thoughout the file
--- map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace the word under cursor throughout the file" })
+keymap("n", "<leader>c", [[:%s/<C-r><C-w>//gc<Left><Left><Left>]], { desc = "Replace word under cursor" })
+-- eymap("n", "<Leader>c", [[:%s/<C-r><C-w>//g<Left><Left>]])
+-- keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace the word under cursor throughout the file" })
 
 -- --tabpage remaps
-map("n", "<leader>ta", "<cmd>tabnew<CR>", { desc = "[T]abpage [A]dd new tab" })
-map("n", "<leader>tr", "<cmd>tabclose<CR>", { desc = "[T]abpage [R]emove current tab" })
-map("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "[T]abpage [N]ext tab" })
-map("n", "<leader>tp", "<cmd>tabprevious<CR>", { desc = "[T]abpage [P]revious tab" })
+keymap("n", "<leader>ta", "<cmd>tabnew<CR>", { desc = "[T]abpage [A]dd new tab" })
+keymap("n", "<leader>tr", "<cmd>tabclose<CR>", { desc = "[T]abpage [R]emove current tab" })
 
 -- Insert a blank line below or above current line (do not move the cursor),
 -- see https://stackoverflow.com/a/16136133/6064933
