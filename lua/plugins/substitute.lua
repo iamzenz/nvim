45c4235f2c37eff53dc00c7ab87effc9355486d7
mike <mikehall@mikehall94.com>
2024-10-05 16:00:56 -0600

    fooling around trying to understand how to use this plugin... no luck

--- lua/plugins/substitute.lua
+++ lua/plugins/substitute.lua
@@ -4,5 +4,13 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-    }
+    },
+    setup = function()
+        local keymap = vim.keymap.set
+        require("substitute").setup()
+        keymap("n", "s", require('substitute').operator)
+        keymap("n", "ss", require('substitute').line)
+        keymap("n", "S", require('substitute').eol)
+        keymap("x", "s", require('substitute').visual)
+    end,
 }
