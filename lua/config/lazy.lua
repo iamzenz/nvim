a293afda24cb9580536afa01c69ff91b93b23746
mike <mikehall@mikehall94.com>
2024-10-09 21:55:46 -0600

    title!

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,3 +1,8 @@
+--          ╔═════════════════════════════════════════════════════════╗
+--          ║                        lazy.nvim                        ║
+--          ╚═════════════════════════════════════════════════════════╝
+
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
