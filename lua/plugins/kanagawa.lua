992e15aea9b9416df110dcb07b55af75158f4ac4
mike <mikehall@mikehall94.com>
2024-10-20 14:46:25 -0600

    enable cursor line highlighting
    curtesy of benfrain

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,5 +1,5 @@
 --          ╔═════════════════════════════════════════════════════════╗
---          ║                       Colorscheme                       ║
+--          ║                      Kanagawa.nvim                      ║
 --          ╚═════════════════════════════════════════════════════════╝
 
 --          ┌─────────────────────────────────────────────────────────┐
@@ -11,20 +11,24 @@
 
 -- ══ TODO: ═══════════════════════════════════════════════════════════
 --
---  - [ ] Set cursorline highlight
---  - [ ] Set CursorLineNr highlight in overrides of config
+--  - [x] Set cursorline highlight
+--  - [x] Set CursorLineNr highlight in overrides of config
 --  - [ ] Do I have all the config options and overrides I need?
 
 
 
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme kanagawa-wave]])
-		end,
-	},
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        dimInactive = true,
+      })
+      vim.cmd([[colorscheme kanagawa-wave]])
+      vim.cmd([[set cursorline]]) -- Enable cursor line highlighting
+    end,
+  },
 }
