75b7738167e4e9197c6a37d7cfdb7efce59e96d9
mike <mikehall@mikehall94.com>
2024-10-20 14:47:09 -0600

    keymap to silence notification while on tiny laptop

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,15 +8,20 @@
 --          └─────────────────────────────────────────────────────────┘
 
 
-return {
+-- ══ TODO: ═══════════════════════════════════════════════════════════
+--
+--  - [ ] Add toggle for notifications
 
-  -- lazy.nvim
-  {
+
+
+
+return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
       -- add any options here
     },
+    -- toggle for notifications
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
@@ -24,7 +29,8 @@ return {
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
-      }
-  }
+    },
 
-}
+    vim.keymap.set("n", "<leader>tn", "<cmd>Noice dismiss<CR>", { desc = "Toggle notifications" })
+
+  }
