828717be314be724accb7895d3b886c10997db81
mike <mikehall@mikehall94.com>
2024-11-16 21:47:04 -0700

    cross platform history would be nice

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -13,6 +13,7 @@
 --  - [ ] I need to migrate the undotree config options from my init.lua to here
 --  - [ ] What other options can I add to the undotree config?
 --  - [ ] Move undodir from .vim to .config/nvim
+--  - [ ] Synch undotree files across all systems
 
 
 return {
