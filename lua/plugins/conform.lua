c5adeeb94b81e69a2af3a152fe0466a51ffa5670
mike <mikehall@mikehall94.com>
2025-01-05 16:31:52 -0700

    removing all markdown formatters. They cramp my style

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,26 +18,6 @@ return {
         local conform = require("conform")
 
         conform.setup({
-            formatters = {
-                ["markdown-toc"] = {
-                    condition = function(_, ctx)
-                        for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
-                            if line:find("<!%-%- toc %-%->") then
-                                return true
-                            end
-                        end
-                        return false
-                    end,
-                },
-                ["markdownlint-cli2"] = {
-                    condition = function(_, ctx)
-                        local diag = vim.tbl_filter(function(d)
-                            return d.source == "markdownlint"
-                        end, vim.diagnostic.get(ctx.buf))
-                        return #diag > 0
-                    end,
-                },
-            },
             formatters_by_ft = {
                 javascript = { "prettier" },
                 typescript = { "prettier" },
@@ -47,8 +27,6 @@ return {
                 html = { "prettier" },
                 json = { "prettier" },
                 yaml = { "prettier" },
-                markdown = { "prettier", "markdownlint-cli2", "markdown-toc" },
-                ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
                 python = { "isort", "black" },
             },
             format_on_save = {
